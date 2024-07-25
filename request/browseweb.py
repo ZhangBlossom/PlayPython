e21d373873e1cd0fd20e1b34e968de302e8d0c15
zhangblossom <460219753@qq.com>
2024-07-25 14:09:27 +0800

    browse

---
+++ request/browseweb.py
@@ -0,0 +1,36 @@
+import requests
+import time
+import random
+
+# 目标URL列表
+urls = [
+    'https://zhangblossom.blog.csdn.net/article/details/137224076?spm=1001.2014.3001.5502',
+    'https://zhangblossom.blog.csdn.net/article/details/137184049?spm=1001.2014.3001.5502'
+]
+
+# User-Agent 列表
+user_agents = [
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 '
+    'Mobile Safari/537.36',
+]
+
+count = 0
+max_attempts = 10  # 最大尝试次数
+
+for attempt in range(max_attempts):
+    try:
+        for url in urls:
+            # 随机选择一个 User-Agent
+            headers = {'User-Agent': random.choice(user_agents)}
+            response = requests.get(url, headers=headers)
+            if response.status_code == 200:
+                count += 1
+                print(f'访问成功 {url}，当前成功次数：{count}')
+            else:
+                print(f'访问失败 {url}，状态码：{response.status_code}')
+        time.sleep(60)  # 休眠时间可以根据需要调整
+    except requests.RequestException as e:
+        print(f'发生网络异常：{e}，将在60秒后重试')
+        time.sleep(60)
